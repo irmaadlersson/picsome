81093c1e184d0bb3da03ec4f4ee9d47e8ab3f434
irmaadlersson <irma.adlersson@humblebee.se>
2020-02-19 12:11:03 +0100

    20-02-19

--- src/Context.js
+++ src/Context.js
@@ -4,6 +4,7 @@ const Context = React.createContext();
 
 function ContextProvider({ children }) {
   const [allPhotos, setAllPhotos] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   const url =
     "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
@@ -13,7 +14,41 @@ function ContextProvider({ children }) {
       .then(data => setAllPhotos(data));
   }, []);
 
-  return <Context.Provider value={{ allPhotos }}>{children}</Context.Provider>;
-}
+  function toggleFavorite(id) {
+    const updatedArr = allPhotos.map(photo => {
+      //skapar en uppdaterad array genom att mappa över alla photon. Den kommer titta på varje photo objekt i den arrayen. Det vi letar efter är att hitta photot som har matchande ID med det som kommer in i denna funktion.
+      if (photo.id === id) {
+        //Om man hittar ID så kommer den att ha alla props från photo FÖRUTOM isFavorite som kommer att ändras till det mottsatta från vad det är just nu.
+        return {
+          ...photo,
+          isFavorite: !photo.isFavorite
+        };
+      } //om detta inte är sant, då kommer vi return photot.
+      return photo;
+    });
+    setAllPhotos(updatedArr);
+  }
 
+  function addToCart(newItem) {
+    setCartItems(prevItems => [...prevItems, newItem]);
+  }
+
+  function removeFromCart(id) {
+    setCartItems(prevItems => prevItems.filter(item => item.id !== id));
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        allPhotos,
+        toggleFavorite,
+        cartItems,
+        addToCart,
+        removeFromCart
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
 export { ContextProvider, Context };
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import { Context } from "../Context";
 import UnFavo from "../icons/unFavorite.svg";
+import Favo from "../icons/Favorite.svg";
 import Add from "../icons/add.svg";
+import Added from "../icons/added.svg";
 
 function Image({ className, img }) {
   const [hovered, setHovered] = useState(false);
-  const heartIcon = hovered && <img src={UnFavo} className="favorite" />;
-  const addIcon = hovered && <img src={Add} className="addCart" />;
+  const { toggleFavorite, addToCart, cartItems, removeFromCart } = useContext(
+    Context
+  );
+  function heartIcon() {
+    if (img.isFavorite) {
+      return (
+        <img
+          src={Favo}
+          className="favorite"
+          onClick={() => toggleFavorite(img.id)}
+        />
+      );
+    } else if (hovered) {
+      return (
+        <img
+          src={UnFavo}
+          className="favorite"
+          onClick={() => toggleFavorite(img.id)}
+        />
+      );
+    }
+  }
+  function cartIcon() {
+    const alreadyInCart = cartItems.some(item => item.id === img.id);
+    if (alreadyInCart) {
+      return (
+        <img
+          src={Added}
+          className="addCart"
+          onClick={() => removeFromCart(img.id)}
+        />
+      );
+    } else if (hovered) {
+      return (
+        <img src={Add} className="addCart" onClick={() => addToCart(img)} />
+      );
+    }
+  }
 
   return (
     <div
@@ -14,10 +54,18 @@ function Image({ className, img }) {
       onMouseLeave={() => setHovered(false)}
     >
       <img src={img.url} className="image-grid" />
-      {heartIcon}
-      {addIcon}
+      {heartIcon()}
+      {cartIcon()}
     </div>
   );
 }
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool
+  })
+};
 
 export default Image;
