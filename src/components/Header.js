c121a07fff3e48940e793ff8ba22b74ff9117e20
irmaadlersson <irma.adlersson@humblebee.se>
2020-02-25 08:51:45 +0100

    0225

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
-import React from "react";
-import Img from "../icons/shopping-cart.png";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import Cart from "../icons/shopping-cart.svg";
+import CartFilled from "../icons/shopping-cart-filled.svg";
+
+import { Context } from "../Context";
 
 function Header() {
+  const { cartItems } = useContext(Context);
+
   return (
     <header>
       <Link to="/">
@@ -10,8 +15,7 @@ function Header() {
       </Link>
 
       <Link to="/cart">
-        {/* <img src={Img} style={{ width: "4%" }} /> */}
-        <h2>Cart</h2>
+        <img src={cartItems.length > 0 ? CartFilled : Cart} />
       </Link>
     </header>
   );
